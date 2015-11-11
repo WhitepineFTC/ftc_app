93853310943ff56e3cbeb1dd3e97e5438b312874
lahvak <jhlavace@svsu.edu>
2015-11-10 21:06:19 -0500

    Testing how to change hardware names

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -60,7 +60,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
     //manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    manager.register ("Robo1Manual", PushBotManual.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
