6a9e0eb47044d58d8254bcb007ecdf982cc34f66
lahvak <jhlavace@svsu.edu>
2015-11-21 11:41:48 -0500

    Make servos work

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeamTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeamTest.java
@@ -2,6 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -12,6 +13,8 @@ public class TeamTest extends OpMode
     private DcMotor v_motor_left_motor;
     private DcMotor v_motor_right_motor;
     private DcMotor v_motor_arm_motor;
+    private Servo v_servo_left_servo;
+    private Servo v_servo_right_servo;
     public TeamTest ()
     {
 
@@ -22,29 +25,61 @@ public class TeamTest extends OpMode
         {
             v_motor_left_motor=hardwareMap.dcMotor.get ("left_motor");
             v_motor_left_motor.setDirection (DcMotor.Direction.REVERSE);
+            telemetry.addData("left moter","found");
         }
+
+
         catch (Exception p_exeption)
         {
             v_motor_left_motor=null;
+            telemetry.addData("left moter","not found");
         }
         try
         {
             v_motor_right_motor=hardwareMap.dcMotor.get ("right_motor");
+            telemetry.addData("right motor","found");
 
         }
         catch (Exception p_exeption)
         {
             v_motor_right_motor=null;
+            telemetry.addData("right motor"," not found");
         }
 
         try
         {
             v_motor_arm_motor=hardwareMap.dcMotor.get ("arm");
+            telemetry.addData("arm","found");
 
         }
         catch (Exception p_exeption)
         {
             v_motor_arm_motor=null;
+            telemetry.addData("arm","not found");
+        }
+
+        try
+        {
+             v_servo_left_servo=hardwareMap.servo.get("left_servo");
+             v_servo_left_servo.setPosition (.5);
+             telemetry.addData("left survo", "found");
+        }
+        catch (Exception p_exception)
+        {
+            v_servo_left_servo=null;
+            telemetry.addData("left survo", "not found");
+        }
+
+        try
+        {
+            v_servo_right_servo=hardwareMap.servo.get("right_servo");
+            v_servo_right_servo.setPosition (.5);
+            telemetry.addData("right survo", "found");
+        }
+        catch (Exception p_exception)
+        {
+            v_servo_right_servo=null;
+            telemetry.addData("right survo", "not found");
         }
     }
 
@@ -57,5 +92,19 @@ public class TeamTest extends OpMode
         v_motor_left_motor.setPower(Range.clip(l_left_motor_power,-1,1));
         v_motor_right_motor.setPower(Range.clip(l_right_motor_power,-1,1));
         v_motor_arm_motor.setPower(Range.clip(l_arm_motor_power,-1,1));
+
+        if (gamepad2.a)
+        {
+            v_servo_left_servo.setPosition(1);
+            v_servo_right_servo.setPosition(0);
+            telemetry.addData("hand","oppen");
+
+        }
+        if (gamepad2.b)
+        {
+            v_servo_left_servo.setPosition(.5);
+            v_servo_right_servo.setPosition(.5);
+            telemetry.addData("hand", "close");
+        }
     }
 }
